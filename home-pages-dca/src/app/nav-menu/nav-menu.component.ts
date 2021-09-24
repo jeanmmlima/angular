b1be044b355b7a51325bb4da9e3341baf44869a5
jeanmmlima <jeanmariomlima@gmail.com>
2021-09-24 11:31:39 -0300

    menu for mobile

--- home-pages-dca/src/app/nav-menu/nav-menu.component.ts
+++ home-pages-dca/src/app/nav-menu/nav-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as M from "materialize-css";
+
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
@@ -10,6 +12,17 @@ export class NavMenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    /*document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = M.Sidenav.init(elems);
+    });*/
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
   }
 
 }
