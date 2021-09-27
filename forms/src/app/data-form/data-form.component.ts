18b369f7e85e23d80c302a580b581eb232cafcee
jeanmmlima <jeanmariomlima@gmail.com>
2021-09-27 16:38:40 -0300

    data-driven form

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -16,9 +16,28 @@ export class DataFormComponent implements OnInit {
   //todo código vai ficar no componente
   formulario: FormGroup;
 
-  constructor() { }
+  //FORMA 2 DE CONSTRUIIR FORM BUILDER
+  constructor(private formBuilder: FormBuilder) {
+
+  }
 
   ngOnInit(): void {
+
+    /*
+    //FORMA 1 DE CONSTRUIR
+    this.formulario = new FormGroup({
+      //iniciando campos do form que são controles do formulario
+      nome: new FormControl('null'), //valor inicial do campo
+      email: new FormControl('null'),
+    });
+    */
+
+    this.formulario = this.formBuilder.group({
+      nome: [null],
+      email: [null],
+    });
+
   }
+
 
 }
